714fd45f7c02c6ff33c1d170e231e49fca9aeb28
NGUYEN HOAI KHUONG <22130128@st.hcmuaf.edu.vn>
2025-04-13 02:59:32 +0700

    Hoàn thành tính năng tính phí giao hàng

--- src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/controller/payment/UpdateAddressServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/controller/payment/UpdateAddressServlet.java
@@ -3,18 +3,11 @@ package vn.edu.hcmuaf.fit.project_final_webcaygiong.controller.payment;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.CustomerDao;
-import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.Customer;
 import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.User;
-import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.diaChi.District;
-import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.diaChi.GHNApiClient;
-import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.diaChi.Ward;
+
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet(name = "UpdateAddressServlet", value = "/UpdateAddress")
 public class UpdateAddressServlet extends HttpServlet {
@@ -32,49 +25,17 @@ public class UpdateAddressServlet extends HttpServlet {
         String district = request.getParameter("districtName");
         String city = request.getParameter("provinceName");
         String ward = request.getParameter("wardName");
-        String prodName = request.getParameter("prodName");
-        String prodQuantity = request.getParameter("prodQuantity");
         int districtId = Integer.parseInt(request.getParameter("districtId"));
         String wardCode = request.getParameter("wardCode");
-
 
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
 
 
         if (user != null) {
-
             // Cập nhật địa chỉ cho khách hàng
-            customerDao.updateCustomerAddress(user.getUserID(), fullName, phoneNumber, address + ", " + ward, district, city);
-            try {
-                JSONObject params = new JSONObject();
-                params.put("service_type_id", 2); // Loại dịch vụ
-                params.put("from_district_id", 1442); // ID quận gửi (ví dụ: Quận 1)
-                params.put("from_ward_code", "21211");
-                params.put("to_district_id", districtId); // ID quận nhận (ví dụ: Quận 4)
-                params.put("to_ward_code", wardCode); // Mã xã/phường (ví dụ: Mã xã)
-                params.put("length", 30); // Chiều dài (cm)
-                params.put("width", 20); // Chiều rộng (cm)
-                params.put("height", 10); // Chiều cao (cm)
-                params.put("weight", 3000); // Trọng lượng (gram)
-                params.put("insurance_value", 0); // Giá trị bảo hiểm
-                params.put("shop_id", 196336); // ID shop của bạn
-
-                // Gọi API để tính phí giao hàng
-                JSONObject result = GHNApiClient.callGHNApi("v2/shipping-order/fee", "POST", params);
-                System.out.println("Response code: " + result.getInt("code"));
-                System.out.println("Response message: " + result.getString("message"));
-                System.out.println("Result: " + result);
-                int service_fee = result.getJSONObject("data").getInt("service_fee");
-                // Lưu thông tin phí giao hàng
-                request.setAttribute("shippingFee", result.getJSONObject("data"));
-                response.sendRedirect("thanhtoan?update=thanhcong" + "&serviceFee=" + service_fee);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                request.setAttribute("error", "Không thể tính phí giao hàng: " + e.getMessage());
-                request.getRequestDispatcher("error.jsp").forward(request, response); // Chuyển đến trang lỗi
-            }
+            customerDao.updateCustomerAddress(user.getUserID(), fullName, phoneNumber, address + ", " + ward, district, city,districtId,wardCode);
+            response.sendRedirect("thanhtoan?update=thanhcong");
         } else {
             response.sendRedirect("login");
         }
--- src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/controller/payment/thanhtoanServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/controller/payment/thanhtoanServlet.java
@@ -3,29 +3,72 @@ package vn.edu.hcmuaf.fit.project_final_webcaygiong.controller.payment;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.CustomerDao;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.cart.Cart;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.cart.CartProduct;
 import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.Customer;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.Product;
 import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.User;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.diaChi.GHNApiClient;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @WebServlet(name = "thanhtoanServlet", value = "/thanhtoan")
 public class thanhtoanServlet extends HttpServlet {
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Lấy thông tin dia chi.
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        if (user != null) {
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (user != null && cart != null) {
             CustomerDao customerDao = new CustomerDao();
             Customer customer = customerDao.getCustomerWithUID(user.getUserID());
             // Kiểm tra giá trị của các biến.
             request.setAttribute("customer", customer);
-            // Hiện thị phí ship.
-            String serviceFeeParam = request.getParameter("serviceFee");
-            Integer serviceFee = serviceFeeParam != null ? Integer.parseInt(serviceFeeParam) : 0;
-            request.setAttribute("serviceFee", serviceFee);
+            // Tính phí vận chuyển.
+            try {
+                int count = 0;
+                JSONObject params = new JSONObject();
+                params.put("from_district_id", 1442);
+                params.put("from_ward_code", "21211");
+                params.put("to_district_id", customer.getDistrictID());
+                params.put("to_ward_code", customer.getWardCode());
+                params.put("insurance_value", 0);
+                params.put("shop_id", 196336);
+                JSONArray items = new JSONArray();
+                for (CartProduct cartProduct : cart.getList()) {
+                    JSONObject item = new JSONObject();
+                    item.put("name", cartProduct.getName());
+                    item.put("quantity", cartProduct.getQuantity());
+                    item.put("length", 20);
+                    item.put("width", 20);
+                    item.put("height", 20);
+                    item.put("weight", 300 * cartProduct.getQuantity());
+                    items.put(item);
+                    count += cartProduct.getQuantity();
+                }
+                params.put("items", items);
+                params.put("weight", 700 * count);
+                params.put("service_type_id", 2);
+                // Gọi API để tính phí giao hàng
+                JSONObject result = GHNApiClient.callGHNApi("v2/shipping-order/fee", "POST", params);
+                System.out.println("Response code: " + result.getInt("code"));
+                System.out.println("Response message: " + result.getString("message"));
+                System.out.println("Result: " + result);
+                int service_fee = result.getJSONObject("data").getInt("service_fee");
+                // Lưu thông tin phí giao hàng
+                request.setAttribute("shippingFee", result.getJSONObject("data"));
+                request.setAttribute("serviceFee", service_fee);
+            } catch (Exception e) {
+                e.printStackTrace();
+                request.setAttribute("serviceFee", 0); // Đặt phí vận chuyển là 0 nếu có lỗi
+            }
 
         }
         RequestDispatcher dispatcher = request.getRequestDispatcher("thanhtoan.jsp");
