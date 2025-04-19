f3b5259e406759b7cbb305ba779f739fff859939
PhanHuynhVyKhan <22130110@st.hcmuaf.edu.vn>
2025-04-19 21:00:50 +0700

    tích hợp api xử lý đăng nhập bằng facebook

---
+++ src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/login/IConstant.java
@@ -0,0 +1,12 @@
+package vn.edu.hcmuaf.fit.project_final_webcaygiong.login;
+
+public class IConstant {
+    public static final String FACEBOOK_CLIENT_ID = "1151500616726229";
+    public static final String FACEBOOK_CLIENT_SECRET = "8203680cb0244a06b8abdfb269ccd767";
+    public static final String FACEBOOK_REDIRECT_URI = "http://localhost:8080/Project_Final_WebCayGiong_war/facebook_login";
+    public static final String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/v19.0/oauth/access_token";
+    public static final String FACEBOOK_LINK_GET_USER_INFO = "https://graph.facebook.com/me?fields=id,name,email,picture&access_token=";
+
+}
+
+
