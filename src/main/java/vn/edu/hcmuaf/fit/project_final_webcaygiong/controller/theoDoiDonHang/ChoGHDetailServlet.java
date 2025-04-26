f36a3dcb78edad82204290fd37349cd01984be7e
NGUYEN HOAI KHUONG <22130128@st.hcmuaf.edu.vn>
2025-04-26 23:22:41 +0700

    Hiện thị thông tin chi tiết của đơn hàng hoàn thành, đơn hàng chờ xác nhận và đơn hàng chờ giao hàng.

---
+++ src/main/java/vn/edu/hcmuaf/fit/project_final_webcaygiong/controller/theoDoiDonHang/ChoGHDetailServlet.java
@@ -0,0 +1,51 @@
+package vn.edu.hcmuaf.fit.project_final_webcaygiong.controller.theoDoiDonHang;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.CustomerDao;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.OrderDao;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.Customer;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.Order;
+import vn.edu.hcmuaf.fit.project_final_webcaygiong.dao.model.User;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "ChoGHDetailServlet", value = "/ChoGHDetail")
+public class ChoGHDetailServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double totalPrice = 0;
+        CustomerDao customerDao = new CustomerDao();
+        OrderDao orderDao = new OrderDao();
+        String oId = request.getParameter("oId");
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        // Kiểm tra xem đã đăng nhập hay chưa.
+        if (user != null) {
+            int userID = user.getUserID();
+            Customer customer = customerDao.getCustomerWithUID(userID);
+            Order order = orderDao.getOrderById(Integer.parseInt(oId));
+            List<Map<String, Object>> orderItems = orderDao.getOrderItemsByOrderID(Integer.parseInt(oId));
+            for (Map<String, Object> item : orderItems) {
+                totalPrice += ((double) item.get("price")) * ((int) item.get("quantity"));
+                System.out.println(totalPrice);
+            }
+            request.setAttribute("orderItems", orderItems);
+            request.setAttribute("customer", customer);
+            request.setAttribute("totalPrice", totalPrice);
+            request.setAttribute("order", order);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("ChoGiaoHangDetail.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("login");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
